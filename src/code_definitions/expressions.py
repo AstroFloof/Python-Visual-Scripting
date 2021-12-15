836c1d7abb52583af158b0abd5fb7ab0b2d64bfd
AstroFloof <does.not.kompute@gmail.com>
2021-12-15 14:10:44 -0500

    big additions to Functions, bug fixes

--- src/code_definitions/expressions.py
+++ src/code_definitions/expressions.py
@@ -1,6 +1,7 @@
 import re
 from keyword import kwlist, softkwlist 
 
+
 # TODO: Move this shit out to staticmethods in pvsclasses
 kwposs = ' |'.join(list(kwlist) + list(softkwlist))
 getwspc = re.compile(r"^(?P<whitespace>[\t ]*).*$", re.I)
@@ -20,7 +21,7 @@ class Expression:
     
     def __init__(self, line: str, line_num: int) -> None:  # TODO: Expression types e.g. variable set, function call
         self._indentation: str = re.match(getwspc, line).groupdict()['whitespace']
-        uses_tabs, uses_spaces = '\t' in self._indentation, '' in self._indentation
+        uses_tabs, uses_spaces = '\t' in self._indentation, ' ' in self._indentation
         if uses_tabs and uses_spaces:
             raise SyntaxError("Inconsistent use of tabs and spaces!")
 
@@ -29,7 +30,7 @@ class Expression:
         self.line_num: int = line_num
 
     def __str__(self):
-        return self.line
+        return str(self.__dict__)
 
 
 AVTC = r"[\w|_\s^\v]+"  # All Valid Typing Characters
@@ -50,11 +51,18 @@ class Argument:
 
     def __init__(self, raw: str):
         match = re.match(self.REGEX, raw.strip())
-        assert match is not None, f"This argument is invalid!\n{raw}"
+        if match is None:
+            assert raw, "This argument is empty!"
+            raise ValueError(f"This argument is invalid!\n{raw}")
 
         result = match.groupdict()
         self.name: str = result.get('name', '').strip()
-        self.is_typed: bool = bool(result.get('has_typehint', False))
-        self.type: list[str] = [union_member.strip() for union_member in result.get('type', '').split("|")]
-        self.is_kwarg: bool = bool(result.get('has_default', False))
-        self.default: str = result.get('default', '')
+        if result.get('has_typehint', False):
+            self.type: list[str] = [union_member.strip() for union_member in result.get('type', '').split("|")]
+        else: 
+            self.type = None
+        
+        if result.get('has_default', False):
+            self.default: str = result.get('default', '')
+        else:
+            self.default = None
